dbe8a4c3dad3351f80de8cccb2543ac7885cb7ad
shiteyaru <andreyuriel4@gmail.com>
2025-01-02 08:30:44 -0300

    Simple Create Form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -19,5 +19,12 @@ namespace SalesWebMvc.Services {
 
         }
 
+        public void Insert(Seller obj) {
+
+            _context.Add(obj);
+            _context.SaveChanges();
+
+        }
+
     }
 }
